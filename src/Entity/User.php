c32ebf47609636797ec440e1488e53b6fa18e296
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-12-20 12:39:48 +0200

    Inicio

---
+++ src/Entity/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Code\Entity;
+
+
+use Code\DB\Entity;
+
+class User extends Entity
+{
+	protected $table = 'users';
+	static $filters = [
+		'first_name' => FILTER_UNSAFE_RAW,
+		'last_name' => FILTER_UNSAFE_RAW,
+		'username' => FILTER_UNSAFE_RAW,
+		'password' => FILTER_UNSAFE_RAW,
+		'password_confirm' => FILTER_UNSAFE_RAW,
+	];
+}
