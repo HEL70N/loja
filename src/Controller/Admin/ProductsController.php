94f89386a2e576d7dcdc151d42ddac9ee0157bf9
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-12-22 08:50:55 +0200

    Entidade Produto e Busca no DB

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -2,13 +2,15 @@
 namespace Code\Controller\Admin;
 
 use Code\View\View;
+use Code\DB\Connection;
+use Code\Entity\User;
 
 class ProductsController
 {
     public function index()
     {
         $view = new View('admin/products/index.phtml');
-        $view->products = [];
+        $view->products = (new Product(Connection::getInstance()))->findAll();
         
         return $view->render();
     }
